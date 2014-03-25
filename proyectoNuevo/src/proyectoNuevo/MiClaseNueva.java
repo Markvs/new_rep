8313e2f10fa7d2621344d6e78dda8c4ef0a3bc2b
Scotty <email@email.com>
2014-03-25 09:24:02 +0100

    Inicializacion

---
+++ proyectoNuevo/src/proyectoNuevo/MiClaseNueva.java
@@ -0,0 +1,12 @@
+package proyectoNuevo;
+
+public class MiClaseNueva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello New World");
+		
+	}
+
+}
